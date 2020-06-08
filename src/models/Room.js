b694abec30c0b687f768d9ec816979c18049eb82
Vangelis Galanis <vaggalanis@gmail.com>
2020-06-08 14:06:16 +0300

    build: update packages

--- src/models/Room.js
+++ src/models/Room.js
@@ -1,8 +1,8 @@
-const uuid = require("uuid");
+const { v4: uuidv4 } = require("uuid");
 
 class Room {
   constructor(votes, conversation, chat) {
-    this.id = uuid.v4();
+    this.id = uuidv4();
     this.votes = votes;
     this.conversation = conversation;
     this.chat = chat;
